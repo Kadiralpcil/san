56226fbbda0c7518defd028883fdb8b8dbab4491
Kadir Alp Çil <kadiralpcil@gmail.com>
2025-07-31 15:00:28 +0300

    fixed import path on api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
-import type { Post } from "../types/Post"
-import type { Comment } from "../types/Comment"
+import type { Post } from "../types"
+import type { Comment } from "../types"
+
 
 export const getPosts = async (start: number = 0, limit: number = 10): Promise<Post[]> => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`)
